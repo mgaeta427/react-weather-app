a8153782e4ab64293886a6b963b658972e181041
mgaeta427 <mgaeta427@gmail.com>
2024-06-21 00:30:45 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-return <div className="App">Weather App</div>
-       
+return (
+<div className="App">
+  <h1>Weather App</h1>
+  <footer>
+    This Project was coded by Maribel Boban and is
+    {" "} 
+  <a 
+  href="https://github.com/mgaeta427/react-weather-app"
+  target="_blank"
+  
+  
+  >
+    open-sourced on GitHub
+    </a>
+  </footer>
+  </div>
+);       
 }
 
