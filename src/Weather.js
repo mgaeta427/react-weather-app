b3463ee5b9a0b7408a3f106073114d5b08a94ebe
mgaeta427 <mgaeta427@gmail.com>
2024-06-25 14:14:09 -0500

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,11 +11,15 @@ export default function Weather() {
             type="seacth" 
             placeholder="Enter a city.."
             className="form-control"
+            autoFocus="on"
              />
              </div>
              <div className="col-3">
-             <input type="submit" value="Search" 
-             className="btn btn-primary" />
+             <input
+              type="submit" 
+              value="Search" 
+             className="btn btn-primary w-100" 
+             />
              </div>
              </div>
         </form>
@@ -24,16 +28,23 @@ export default function Weather() {
             <li>Wednesday 07:00</li>
             <li>Mostly Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
         <div className="col-6">
+        <div className="clearfix">
+        
             <img 
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" 
             alt="Mostly Cloudy"
+            className="float-left"
             />
-            23°C
-        </div>
-        <div className="col-6">
-        <ul>
+            
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+            
+            </div>
+            </div>
+            <div className="col-6">
+            <ul>
             <li>Precipitation: 0%</li>
             <li>Humidity: 56%</li>
             <li>Wind: 5km/h</li>    
